cdcdfabb18f85595a68b97366b2bb25e7a719871
Hiropython <inthemountain4@gmail.com>
2024-11-12 02:30:01 +0000

    finish obediant_...

--- projects/06_Projects/obedient_turtle.py
+++ projects/06_Projects/obedient_turtle.py
@@ -7,6 +7,13 @@ from guizero import App, Box, Text, TextBox, PushButton, ListBox, error
 
 
 # TODO)
+import turtle
+turtle.setup(width=600, height=600)
+t=turtle.Turtle()
+t.shape('turtle')
+
+
+
 #   1. Create a turtle.
 #   2. Write 3 function definitions for drawing a square, triangle, and
 #      circle.
@@ -14,3 +21,4 @@ from guizero import App, Box, Text, TextBox, PushButton, ListBox, error
 #   4. Draw the appropriate shape depending on their answer (call the right
 #      function)
 pass
+turtle.exitonclick()
